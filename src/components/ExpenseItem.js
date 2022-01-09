aa36df07e0663d6e916f92f86453bc34a7fed2b6
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-09 19:39:08 -0300

    Passando dados via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-
-    const expenseTitle = 'Conta de água';
-    const expenseDate = '08/01/2022';
-    const expenseAmount = 265.00;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{ expenseDate }</div>
+            <div>{ props.date.toISOString() }</div>
             <div className="expense-item__description">
-                <h2>{ expenseTitle }</h2>
-                <div className="expense-item__price">R$ { expenseAmount }</div>
+                <h2>{ props.title }</h2>
+                <div className="expense-item__price">R$ { props.amount }</div>
             </div>
         </div>
     );
