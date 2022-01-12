273ecc7c39af2231637b9385535752d08957622b
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-11 22:42:09 -0300

    Reorganizando a estrutura de pastas

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css';
 
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseItem__date';
 
 function ExpenseItem(props) {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import './Expenses.css';
 
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props){
