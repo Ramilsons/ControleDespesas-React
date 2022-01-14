8458a1402c77cd5d66d6fa3c76756923230fa5bd
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-13 21:21:17 -0300

    Trabalhando com o hook useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
 import './ExpenseItem.css';
 
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseItem__date';
 
 function ExpenseItem(props) {
+
+    // o useState deve receber com parametro o valor padrão
+    const [title, setTitle] = useState(props.title);
+    // o useSate retorna um array com dois elementos. O primeiro é o valor atual(nesse caso chamamos de title)
+    // o segundo elemento é a função que atualizará o valor, nesse caso chamamos de setTitle
+
+    function changeTitle(){
+        setTitle('Título atualizado pelo useState');
+    }
 
     return (
         // chamando o container que criamos
         <Card className="expense-item">
             <ExpenseDate date={props.date} /> 
             <div className="expense-item__description">
-                <h2>{ props.title }</h2>
+                <h2>{ title }</h2>
                 <div className="expense-item__price">R$ { props.amount }</div>
             </div>
+
+            <button onClick={ changeTitle }>Atualizar título</button>
         </Card>
     );
 }
