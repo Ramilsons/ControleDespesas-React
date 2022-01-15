41d92cd98db11fb80fa3e2d84bb243842ea77ed1
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-14 23:05:12 -0300

    Chamandos varias useState no mesmo bloco

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,39 +4,55 @@ import { useState } from 'react';
 
 function ExpenseForm(){
     
+    /* Chamando de forma individual  
+   
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState(''); */
 
-    
+    /* Chamando múltiplos no mesmo bloco */
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredDate: '',
+        enteredAmount: ''
+    });
+
     function titleInputChangedHandler(event){
-        setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        });
     }
 
     function amountInputChangedHandler(event){
-        setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        });
     }
 
     function dateInputChangedHandler(event){
-        setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        });
     }
-
 
 
     return (
         <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
-            <label for='titleInput'>Título</label>
-            <input type='text' id='titleInput' onChange={ titleInputChangedHandler } />
+                <label for='titleInput'>Título</label>
+                <input type='text' id='titleInput' onChange={ titleInputChangedHandler } />
             </div>
             <div className='new-expense__control'>
-            <label for='amountInput'>Valor</label>
-            <input type='number' id='amountInput' min='0.01' step='0.01' onChange={ amountInputChangedHandler } />
+                <label for='amountInput'>Valor</label>
+                <input type='number' id='amountInput' min='0.01' step='0.01' onChange={ amountInputChangedHandler } />
             </div>
             <div className='new-expense__control'>
-            <label for='dateInput'>Data</label>
-            <input type='date' id='dateInput' min='2022-01-01' max='2022-12-31'  onChange={ dateInputChangedHandler } />
+                <label for='dateInput'>Data</label>
+                <input type='date' id='dateInput' min='2022-01-01' max='2022-12-31'  onChange={ dateInputChangedHandler } />
             </div>
         </div>
         <div className='new-expense__actions'>
