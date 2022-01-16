cea60c4c3613ab43add648b35528823c6995132b
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-16 14:06:39 -0300

    Passando dados do componente filho(ExpenseForm) para o componente pai(NewExpense)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,62 +2,72 @@ import './ExpenseForm.css';
 
 import { useState } from 'react';
 
-function ExpenseForm(){
+function ExpenseForm(props){
     
-    /* Chamando de forma individual  
-   
+    /* Chamando de forma individual  */
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState(''); */
+    const [enteredAmount, setEnteredAmount] = useState(''); 
 
-    /* Chamando múltiplos no mesmo bloco */
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredDate: '',
-        enteredAmount: ''
-    });
+    /* Chamando múltiplos no mesmo bloco 
+        const [userInput, setUserInput] = useState({
+            enteredTitle: '',
+            enteredDate: '',
+            enteredAmount: ''
+        }); 
+    */
 
-    function titleInputChangedHandler(event){
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        });
-    }
+    function titleInputChangedHandler(event){     
+        setEnteredTitle(event.target.value)
+    };
 
     function amountInputChangedHandler(event){
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        });
-    }
+        setEnteredAmount(event.target.value)
+    };
 
     function dateInputChangedHandler(event){
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        });
-    }
+        setEnteredDate(event.target.value);
+    };
 
+    function submitHandler(event){
+        // fazendo com que não ocorra o reload da página(comportamento padrão)
+        event.preventDefault();
+
+        const expenseDate = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        // acessando a função configurada no pai e passando os dados que o usuário preencheu no form
+        props.onSaveExpenseDate(expenseDate);
+
+        // limpando os campos para uma UX melhor
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
 
     return (
-        <form>
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label for='titleInput'>Título</label>
-                <input type='text' id='titleInput' onChange={ titleInputChangedHandler } />
+        // onSubmit ocorre quando o usuário clica e tenta enviar o formulário
+        <form onSubmit={ submitHandler }>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label htmlFor='titleInput'>Título</label>
+                    <input value={ enteredTitle } type='text' id='titleInput' onChange={ titleInputChangedHandler } />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor='amountInput'>Valor</label>
+                    <input value={ enteredAmount } type='number' id='amountInput' min='0.01' step='0.01' onChange={ amountInputChangedHandler } />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor='dateInput'>Data</label>
+                    <input value={ enteredDate } type='date' id='dateInput' min='2022-01-01' max='2022-12-31'  onChange={ dateInputChangedHandler } />
+                </div>
             </div>
-            <div className='new-expense__control'>
-                <label for='amountInput'>Valor</label>
-                <input type='number' id='amountInput' min='0.01' step='0.01' onChange={ amountInputChangedHandler } />
+            <div className='new-expense__actions'>
+                <button type='submit'>Adicionar Despesa</button>
             </div>
-            <div className='new-expense__control'>
-                <label for='dateInput'>Data</label>
-                <input type='date' id='dateInput' min='2022-01-01' max='2022-12-31'  onChange={ dateInputChangedHandler } />
-            </div>
-        </div>
-        <div className='new-expense__actions'>
-            <button type='submit'>Adicionar Despesa</button>
-        </div>
         </form>
     );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,21 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(){
+
+    // recebendo o objeto que foi criado no componente filho no parametro
+    function saveExpenseDataHandler(enteredExpenseDate){
+        const expensedDate = {
+            ...enteredExpenseDate,
+            id: Math.random().toString() // adicionando um id junto ao objeto recebido
+        }
+
+        console.log(expensedDate);
+    }
+
     return(
         <div className='new-expense'>
-            <ExpenseForm />
+            { /* Abaixo criamos um listener personalizado, ele será o responsavel por fazer a comunição dos dados do filho para o pai */ }
+            <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
         </div>
     )
 }
