71c105729dddc9cbc7744dd92dfab6f883404578
Ramilsons <ramilsonfelixsilva9@gmail.com>
2022-01-18 20:52:14 -0300

    Exibindo conteúdo de acordo com uma condição

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,14 +10,31 @@ function Expenses(props){
     const [yearFilter, setYearFilter] = useState('2022');
 
     function yearChangeHandler(yearSelected){
-        setYearFilter(yearSelected)
+        setYearFilter(yearSelected);
     }
 
+    let allExpensesFiltered = props.items.filter(expense => expense.date.getFullYear().toString() === yearFilter);
+   
     return(     
         <Card className="expenses">
             <ExpensesFilter selected={yearFilter} onYearChange={ yearChangeHandler }/>
-            {/* rendenrizando lista de dados */}
-            {props.items.map(expense => <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} /> )}
+            {
+            /* rendenrizando lista de dados com condição */
+            allExpensesFiltered.length == 0 ? <p>Não há despesas nesse ano 🎊</p> : allExpensesFiltered.map(expense => (
+                    /* 
+                        Devemos sempre adicionar key quando estamos fazendo o map em alguma lista. 
+                        As chaves ajudam o React a identificar quais itens foram alterados, adicionados ou removidos 
+                        Na maioria das vezes, você usaria IDs de seus dados como key
+                    */
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title} 
+                        date={expense.date}
+                        amount={expense.amount} 
+                    /> 
+                ))
+            }
+
         </Card>
     )
 };
